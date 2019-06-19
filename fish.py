88571cd2b03ed72d0146c6ef07c4d5299c4f6bfc
Manuel Sande <mnlsande22@gmail.com>
2019-06-19 08:07:39 -0400

    create constructor of fish class & accessor methods

--- fish.py
+++ fish.py
@@ -1,2 +1,33 @@
 import cTurtle
 from world import *
+
+class Fish:
+    """
+    Fish should know: i) the world they belong to, ii) their specific
+    location(x,y) iii) and how long they have gone without breeding.
+    iv) Each fish also has an instance of Turtle so it can be displayed.
+    """
+
+    def __init__(self):
+        self.turtle = cTurtle.Turtle()
+        self.turtle.up()
+        self.turtle.hideturtle()
+        self.turtle.shape("Fish.gif")
+
+        self.xpos = 0
+        self.ypos = 0
+        self.world = None
+
+        self.breedTick = 0
+
+# Accessor methods to interact with Fish objects.
+# "getting" and "setting" values.
+    def setX(self, newx):
+        self.xpos = newx
+    def setY(self, newy):
+        self.ypos = newy
+
+    def getX(self):
+        return self.xpos
+    def getY(self):
+        return self.ypos
