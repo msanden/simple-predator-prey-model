5ab15349b591828641306a1e7e1fec7db1c64e59
Manuel Sande <mnlsande22@gmail.com>
2019-06-20 02:56:38 -0400

    Add comment, add livelife() method

--- fish.py
+++ fish.py
@@ -3,9 +3,9 @@ from world import *
 
 class Fish:
     """
-    Fish should know: i) the world they belong to, ii) their specific
-    location(x,y) iii) and how long they have gone without breeding.
-    iv) Each fish also has an instance of Turtle so it can be displayed.
+    Fish should know: i.) the world they belong to, ii.) their specific
+    location(x,y) iii.) and how long they have gone without breeding.
+    iv.) Each fish also has an instance of Turtle so it can be displayed.
     """
 
     def __init__(self):
@@ -49,4 +49,29 @@ class Fish:
         self.ypos =  newy
         self.turtle.goto(self.xpos, self.ypos)
 
-    
+    def liveLife(self):
+        '''
+        A fish checks the number of fish in its surrounding locations, it
+        dies if there's overcrowding or breeds and moves when there's no
+        crowding.
+        '''
+        offsetList = [(-1,1), (0,1), (1,1),
+                      (-1,0),        (1,0),
+                      (-1,-1),(0,-1),(1,-1)]
+        adjfish = 0
+        for offset in offsetList:
+            newx = self.xpos + offset[0]
+            newy = self.ypos + offset[1]
+            if 0 <= newx < self.world.getXDimension() and
+               0 <= newx < self.world.getYDimension():
+               if (not self.world.emptyLocation(newx,newy)) and
+                  isinstance(self.world.lookAtLocation(newx, newy, Fish)):
+                   adjfish = adjfish + 1
+
+        if adjfish >= 2:
+            self.world.delLifeForm(self)
+        else:
+            self.breedTick = self.breedTick + 1
+            if self.breedTick >= 12:
+                self.tryToBreed()
+            self.tryToMove()
