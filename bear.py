4591d14a7a4a6e09095ac1a5600794ce41d579cb
Manuel Sande <mnlsande22@gmail.com>
2019-06-20 06:27:28 -0400

    Implement livelife and tryToEat methods

--- bear.py
+++ bear.py
@@ -1,4 +1,8 @@
-class Bear(object):
+import cTurtle
+from world import *
+from fish import *
+
+class Bear:
     """In this context, bears will first and foremost try to breed.  They
     will eat fish if they are available, and if any bear does not get enough
     food it will starve and die."""
@@ -15,4 +19,40 @@ class Bear(object):
         self.turtle.hideturtle()
         self.turtle.shape("Bear.gif")
 
-        
+    def liveLife(self):
+        self.breedTick = self.breedTick + 1
+        if self.breedTick >= 8:
+            self.tryToBreed()
+
+        self.tryToEat()
+
+        if self.starveTick == 10:
+            self.world.delLifeForm(self)
+        else:
+            self.tryToMove()
+
+    def tryToEat(self):
+        offsetList = [(-1,1), (0,1), (1,1),
+                      (-1,0),        (1,0),
+                      (-1,-1),(0,-1),(1,-1)]
+        adjprey = []
+        for offset in offsetList:
+            newx = self.xpos + offset[0]
+            newy = self.ypos + offset[1]
+            if 0 <= newx < self.world.getXDimension() and
+               0 <= newx < self.world.getYDimension():
+               if (not self.world.emptyLocation(newx,newy)) and
+               isinstance(self.world.lookAtLocation(newx, newy, Fish)):
+                   adjprey.append(self.world.lookAtLocation(newx, newy))
+
+        if len(adjprey) > 0:
+            randomprey = adjprey[random.randrange(len(adjprey))]
+            preyx = randomprey.getX()
+            preyy = randomprey.getY()
+
+            self.world.delLifeForm(randomprey)
+            self.move(preyx, preyy)
+            self.starveTick = 0
+        else:
+            self.starveTick = self.starveTick + 1
+            
