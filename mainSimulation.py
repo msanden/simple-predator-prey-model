aaf6b9a35eed286af773472ebddf4b5eef745ac3
Manuel Sande <mnlsande22@gmail.com>
2019-06-20 09:47:56 -0400

    create simulation method

--- mainSimulation.py
+++ mainSimulation.py
@@ -1,4 +1,38 @@
 import cTurtle
+import random
+
 from world import *
 from fish import *
 from bear import *
+
+
+def mainSimulation():
+    numberOfBears = 10
+    numberOfFish = 10
+    worldLifeTime = 2500
+    worldWidth = 50
+    worldLength = 25
+
+    myworld = World(worldWidth, worldLength)
+    myworld.draw()
+
+    for fish in range(numberOfFish):
+        newfish = Fish()
+        x = random.randrange(myworld.getXDimension())
+        y = random.randrange(myworld.getYDimension())
+        while not myworld.emptyLocation(x,y):
+            x = random.randrange(myworld.getXDimension())
+            y = random.randrange(myworld.getYDimension())
+        myworld.addLifeForm(newfish, x, y)
+
+    for bear in range(numberOfBears):
+        newbear = Bear()
+        x = random.randrange(myworld.getXDimension())
+        y = random.randrange(myworld.getYDimension())
+        while not myworld.emptyLocation(x,y):
+            x = random.randrange(myworld.getXDimension())
+            y = random.randrange(myworld.getYDimension())
+        myworld.addLifeForm(newbear, x, y)
+
+    for time in range(worldLifeTime):
+        myworld.liveLife()
